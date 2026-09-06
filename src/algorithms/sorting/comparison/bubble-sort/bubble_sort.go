84a4394b38eb8cfdd8e442bb42692a44386f2955
Alexander Schofield <alex.schofield816@gmail.com>
2026-09-06 11:50:25 -0400

    bubble sort

--- src/algorithms/sorting/comparison/bubble-sort/bubble_sort.go
+++ src/algorithms/sorting/comparison/bubble-sort/bubble_sort.go
@@ -1,1 +1,23 @@
 package bubble_sort
+
+import (
+	"errors"
+)
+
+var ErrNilComparator = errors.New("function requires a non nil compare function.")
+
+func BubbleSort[T any](items []T, compare func(T, T) int) (bool, error) {
+	if compare == nil {
+		return false, ErrNilComparator
+	}
+
+	for i := 0; i < len(items); i++ {
+		for j := i; j < len(items); j++ {
+			if compare(items[j], items[j+1]) < 0 {
+				items[j+1], items[j] = items[j], items[j+1]
+			}
+		}
+	}
+
+	return true, nil
+}
