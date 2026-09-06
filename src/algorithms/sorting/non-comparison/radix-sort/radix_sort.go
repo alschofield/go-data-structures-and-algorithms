c60100d2dcc2f636fbc18c3a68a6dfaf4c5b9670
Alexander Schofield <alex.schofield816@gmail.com>
2026-09-06 14:37:43 -0400

    radix sort scaffold and graph interface

--- src/algorithms/sorting/non-comparison/radix-sort/radix_sort.go
+++ src/algorithms/sorting/non-comparison/radix-sort/radix_sort.go
@@ -1,1 +1,7 @@
 package radix_sort
+
+func RadixSort(items []uint32) bool {
+	// counting sort 4(?) times on byte ranges?
+	// or should/can it be different from the C strategy?
+	return false
+}
--- src/data-structures/graphs/graph/graph.go
+++ src/data-structures/graphs/graph/graph.go
@@ -1,1 +1,11 @@
 package graph
+
+import "errors"
+
+var ErrInvalidIndex error = errors.New("function requires a valid index.")
+
+type Graph interface {
+	Directed() bool
+	VertexCount() int
+	Neighbors(vertex int, visit func(neighbor int, weight int64) (bool, error))
+}
