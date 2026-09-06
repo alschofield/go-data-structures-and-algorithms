0e34a5db9ccccb6302ef222c1c74632f09b05695
Alexander Schofield <alex.schofield816@gmail.com>
2026-09-06 14:18:05 -0400

    counting sort should be something like this

--- src/algorithms/sorting/non-comparison/counting-sort/counting_sort.go
+++ src/algorithms/sorting/non-comparison/counting-sort/counting_sort.go
@@ -1,1 +1,29 @@
 package counting_sort
+
+import (
+	"errors"
+)
+
+var ErrKeyOutOfRange error = errors.New("function needs a key that is within range.")
+
+func CountingSort(items []uint32, keyLimit uint32) (bool, error) {
+	if len(items) <= int(keyLimit) {
+		return false, ErrKeyOutOfRange
+	}
+
+	var keys []int
+	for i := 0; i < len(items); i++ {
+		keys[items[i]]++
+	}
+
+	for i, j := 0, 0; i < len(keys); {
+		if keys[i] != 0 {
+			items[j] = uint32(i)
+			j++
+		} else {
+			i++
+		}
+	}
+
+	return true, nil
+}
